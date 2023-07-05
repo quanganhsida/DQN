45bdd80db1aff99f501fdb1ae415af5e0ec72c9a
quanganhsida <anh.tq198162@sis.hust.edu.vn>
2023-07-05 18:38:39 +0700

    commit DQN

---
+++ test_agent.py
@@ -0,0 +1,25 @@
+import gym
+import keras 
+import numpy as np
+
+env = gym.make('CartPole-v1', render_mode = 'human')
+state, _ = env.reset()
+state_size = env.observation_space.shape[0]
+
+#Load the agent
+my_agent = keras.models.load_model('train_agent.h5')
+n_timesteps = 500
+total_reward = 0
+
+for t in range(n_timesteps):
+    env.render()
+    #Put the current state into the predict
+    state = state.reshape((1, state_size))
+    q_values = my_agent.predict(state, verbose=0)
+    max_q_values = np.argmax(q_values)
+
+    #Take action into env
+    next_state, reward, terminal, _, _ = env.step(action=max_q_values)
+    total_reward += reward
+    state = next_state
+    print(t)
